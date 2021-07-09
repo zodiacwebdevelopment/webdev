cb2fbc951597d01cba70dcfd3d274ea1b0a71f02
zodiacwebdevelopment <61382669+zodiacwebdevelopment@users.noreply.github.com>
2021-07-09 10:13:24 -0500

    Add files via upload

--- docs/online unit converter/script.js
+++ docs/online unit converter/script.js
@@ -1,6 +1,8 @@
-let inputs = document.getElementsByClassName("input");
+let pInputs = document.getElementsByClassName("pInput");
+let dInputs = document.getElementsByClassName("dInput");
 
-let factors = [1,0.10194,0.010194,101.904,0.00987,7.50062,7.50062,0.145038,20.8854,0.01];
+let pFactors = [1,0.10194,0.010194,101.904,0.00987,7.50062,7.50062,0.145038,20.8854,0.01];
+let dFactors = [1,10,5,0.5556];
 
 function roundTo(number, decimals){
   let power = Math.pow(10, decimals);
@@ -10,16 +12,20 @@ function roundTo(number, decimals){
   return rounded;
 }
 
-function returnHandler(index){
+function returnHandler(index, array, arrayFactors){
   return function(){
-    let value = Number(inputs[index].value);
-    for(j=0; j<inputs.length; j++){
-      let aValue = value * factors[j]/factors[index];
-      inputs[j].value = roundTo(aValue,5);
+    let value = Number(array[index].value);
+    for(j=0; j<array.length; j++){
+      let aValue = value * arrayFactors[j]/arrayFactors[index];
+      array[j].value = roundTo(aValue,5);
     }
   }
 }
 
-for(i=0; i<inputs.length; i++){
-  inputs[i].addEventListener('change', returnHandler(i));
+for(i=0; i<pInputs.length; i++){
+  pInputs[i].addEventListener('change', returnHandler(i,pInputs,pFactors));
+}
+
+for(i=0; i<dInputs.length; i++){
+  dInputs[i].addEventListener('change', returnHandler(i,dInputs,dFactors));
 }
